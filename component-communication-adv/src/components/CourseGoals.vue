d5da80d6a78e1e2e57a6a73fe9717cfd1b6830bf
Jen Zhu <jennifer0zhu@gmail.com>
2021-09-26 17:25:11 -0400

    adding advanced component communication before moving onto dynamic components

---
+++ component-communication-adv/src/components/CourseGoals.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul v-for="goal in goals" :key="goal">
+    <slot :item="goal" another-prop="..."></slot>
+  </ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      goals: ['finish course', 'learn vue']
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
