3aed4a06a6cf7c2279f5381cc5ba27208f4a94b4
Jen Zhu <jennifer0zhu@gmail.com>
2021-09-26 18:56:40 -0400

    changes after learning about modals and custom dialogs

--- component-communication-adv/src/App.vue
+++ component-communication-adv/src/App.vue
@@ -1,31 +1,24 @@
 <template>
   <div>
     <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
-    <course-goals #default="slotProps">
-      <h2>{{slotProps.item}}</h2>
-      <p>{{slotProps['another-prop']}}</p>
-    </course-goals>
+    <button @click="setSelectedComponent('active-goals')">Active Goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage Goals</button>
+    <keep-alive>
+      <component :is="selectedComp"></component>
+    </keep-alive>
   </div>
 </template>
 
 <script>
-import TheHeader from './components/TheHeader.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
-import CourseGoals from './components/CourseGoals.vue';
+import TheHeader from './components/layout/TheHeader.vue';
+import ActiveGoals from './components/goal-related/ActiveGoals.vue';
+import ManageGoals from './components/goal-related/ManageGoals.vue';
 
 export default {
   components: {
     TheHeader,
-    BadgeList,
-    UserInfo,
-    CourseGoals
+    ActiveGoals,
+    ManageGoals
   },
   data() {
     return {
@@ -34,8 +27,14 @@ export default {
         description: 'Site owner and admin',
         role: 'admin',
       },
+      selectedComp: 'active-goals'
     };
   },
+  methods: {
+    setSelectedComponent(componentName) {
+      this.selectedComp = componentName;
+    }
+  }
 };
 </script>
 
---
+++ component-communication-adv/src/components/goal-related/ManageGoals.vue
@@ -0,0 +1,39 @@
+<template>
+  <h2>Manage Goals</h2>
+  <input type="text" ref="goal"/>
+  <button @click="setGoal">Set Goal</button>
+  <teleport to="body">
+    <error-alert v-if="inputIsInvalid">
+      <h2>Input is Invalid!</h2>
+      <p>Please enter a few characters...</p>
+      <button @click="confirmError">Okay</button>
+    </error-alert>
+  </teleport>
+</template>
+
+<script>
+import ErrorAlert from "../layout/ErrorAlert";
+export default {
+  components: {ErrorAlert},
+  data(){
+    return {
+      inputIsInvalid: false
+    }
+  },
+  methods: {
+    setGoal() {
+      const enteredVal = this.$refs.goal.value;
+      if (enteredVal === '') {
+        this.inputIsInvalid = true;
+      }
+    },
+    confirmError() {
+      this.inputIsInvalid = false;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
